0bfad57bd6d40b3fdd65bd9004c02b9127eb3b7c
psycake <52770775+psycake@users.noreply.github.com>
2023-05-13 14:17:43 -0600

    Add CUD resolvers for brandSocial table

---
+++ src/resolvers/brandSocial/update.ts
@@ -0,0 +1,24 @@
+import { iContext } from "index";
+
+export const updateBrandSocial = async (
+	parent: any,
+	args: {
+		id: number;
+		input: {
+			fkBrand?: number;
+			fkSocialNetwork?: number;
+			username?: string;
+			creationDate?: string;
+		};
+	},
+	{ db }: iContext,
+	info: any
+) => {
+	const toUpdate = await db.sequelize.models.BrandSocial.findByPk(args.id);
+
+	if (!toUpdate) {
+		throw new Error(`No entry with the id ${args.id} could be found`);
+	} else {
+		return await toUpdate.update(args.input);
+	}
+};
