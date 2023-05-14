b9a44d7f99cad5e30deb1b448a3070b1223dc9b4
psycake <52770775+psycake@users.noreply.github.com>
2023-05-13 21:29:40 -0600

    Add a first try for nesting resolving; doesn't work

--- src/resolvers/brandSocial/read.ts
+++ src/resolvers/brandSocial/read.ts
@@ -1,6 +1,7 @@
 import { iContext } from "index";
 import { getFields } from "../../utils/getFields";
 import { iBrandSocial } from "types";
+import { readSocialNetwork } from "../socialNetwork/read"
 
 export const readBrandSocial = async (
 	parent: any,
@@ -8,7 +9,13 @@ export const readBrandSocial = async (
 	{ db }: iContext,
 	info: any
 ): Promise<iBrandSocial[]> => {
+	console.log("info arg:");
+	console.log(info);
+
 	const fields = getFields(info, "readBrandSocial");
+
+	console.log("fields obj:");
+	console.log(fields);
 
 	const brandFields = fields.include.find(
 		(brand) => brand.name === "brand"
@@ -17,6 +24,26 @@ export const readBrandSocial = async (
 		(social) => social.name === "socialNetwork"
 	);
 
+	console.log("brandFields obj:");
+	console.log(brandFields);
+	console.log("socialNetworkFields obj:");
+	console.log(socialNetworkFields);
+	
+	const entries = (await db.sequelize.models.BrandSocial.findAll({
+    	attributes: fields.attributes
+	})) as any[];
+
+	if (socialNetworkFields) {
+		entries.map(async (entry) => {
+			entry.socialNetwork = await readSocialNetwork(this, { id: entry.id }, { db }, info);
+			console.log("entry.socialNetwork contains:");
+			console.log(entry.socialNetwork);
+		})
+	}
+
+	return entries;
+
+	/**
 	const searchedId = (!args.id) ? undefined : { id: args.id }
 	const read = (await db.sequelize.models.BrandSocial.findAll({
 		where: searchedId,
@@ -36,4 +63,5 @@ export const readBrandSocial = async (
 	})) as any[];
 
 	return read;
+	*/
 };
--- src/resolvers/socialNetwork/read.ts
+++ src/resolvers/socialNetwork/read.ts
@@ -5,7 +5,7 @@ export const readSocialNetwork = async (
 	args: { id: number },
 	{ db }: iContext,
 	info: any
-) => {
+): Promise<any[]> => {
 	const read = !args.id ? await db.sequelize.models.SocialNetwork.findAll() : [await db.sequelize.models.SocialNetwork.findByPk(args.id)]
 	return read
 };
