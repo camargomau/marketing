60c9db3733671abe8335e00f9fc5f56aa25bef20
psycake <52770775+psycake@users.noreply.github.com>
2023-05-14 22:33:40 -0600

    Add delete page

---
+++ pages/scripts/deleteEntry.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const forms = document.querySelectorAll("form[data-table]")
+
+	forms.forEach((form) => {
+		form.addEventListener("submit", function (event) {
+			event.preventDefault()
+
+			const table = form.dataset.table
+			const tableDiv = document.querySelector(`#${table}Table`)
+
+			const mutation = `
+				mutation($searchedId: Int!) {
+					delete${ table.charAt(0).toUpperCase() + table.slice(1) }(id: $searchedId) {
+						id
+          			}
+        		}
+      		`
+			const searchedId = document.querySelector(`#${table}Id`).value
+				? parseInt(document.querySelector(`#${table}Id`).value)
+				: null
+
+			fetch("http://localhost:4000/graphql", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					query: mutation,
+					variables: { searchedId: searchedId }
+				})
+			})
+				// Parse the response as JSON
+				.then((response) => response.json())
+				// Put the JSON table into the div
+				.then((data) => {
+					console.log(data)
+
+					// Error handling's different from normal queries
+					if (!data.errors) {
+						tableDiv.innerHTML = `
+						<div class="placeholder">
+							<h4>Success: entry with ID ` + searchedId + ` deleted</h4>
+						</div>
+						`
+					} else {
+						tableDiv.innerHTML = `
+						<div class="placeholder">
+							<h4>Error: check console for more details</h4>
+						</div>
+						`
+					}
+				})
+		})
+	})
+})
