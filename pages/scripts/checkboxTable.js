4a591847b279dc364de24b799bd94e85424e4d4d
psycake <52770775+psycake@users.noreply.github.com>
2023-05-14 18:33:56 -0600

    Add socialNetworks table and styling

--- pages/scripts/checkboxTable.js
+++ pages/scripts/checkboxTable.js
@@ -1,37 +1,52 @@
+// Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-	const form = document.getElementById("tierForm")
+	// Store all forms with the data-table attribute
+	const forms = document.querySelectorAll("form[data-table]")
 
-	form.addEventListener("submit", function (event) {
-		event.preventDefault() // Prevent the default form submission behavior
+	// Loop through all forms
+	forms.forEach((form) => {
+		form.addEventListener("submit", function (event) {
+			// Prevent the form from submitting and reloading the page
+			event.preventDefault()
 
-		const checkboxes = form.querySelectorAll('input[type="checkbox"]:checked')
-		const columns = Array.from(checkboxes, (checkbox) => checkbox.value)
+			// data-table stores the table name that will be used to query
+			const table = form.dataset.table
+			// Find the corresponding div for the table
+			const tableDiv = document.querySelector(`#${table}Table`)
 
-		const query = `
-		query {
-			readTier {
-			${columns.join("\n")}
-			}
-		}
-	`
+			// Store all checked checkboxes
+			const checkboxes = form.querySelectorAll('input[type="checkbox"]:checked')
+			// Convert into an array of column names
+			const columns = Array.from(checkboxes, (checkbox) => checkbox.value)
 
-		fetch("http://localhost:4000/graphql", {
-			// Send a POST request to the desired URL
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				query: query
+			const query = `
+        query {
+          read${table.charAt(0).toUpperCase() + table.slice(1)} {
+            ${columns.join("\n")}
+          }
+        }
+      `
+
+			// Send a POST request to the GraphQL server with the query string in the body
+			fetch("http://localhost:4000/graphql", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					query: query
+				})
 			})
+				// Parse the response as JSON
+				.then((response) => response.json())
+				// Put the JSON table into the div
+				.then((data) => {
+					console.log(data)
+					tableDiv.innerHTML = jsonToTable(data)
+				})
+				.catch((error) => {
+					console.error(error)
+				})
 		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data) // Handle the response returned by the server, and do something with it
-				document.write(jsonToTable(data))
-			})
-			.catch((error) => {
-				console.error(error)
-			})
 	})
 })
--- pages/scripts/jsonToTable.js
+++ pages/scripts/jsonToTable.js
@@ -1,29 +1,29 @@
 const jsonToTable = (rawJson) => {
-  const tableJson = Object.values(rawJson.data)[0];
+	const tableJson = Object.values(rawJson.data)[0]
 
-  const createTable = (tableJson) => {
-    let tableHtml = "<table><tr>";
+	const createTable = (tableJson) => {
+		let tableHtml = "<table><tr>"
 
-    // Add row of headers
-    for (const header of Object.keys(tableJson[0])) {
-      tableHtml += `<th>${header}</th>`;
-    }
-    tableHtml += "</tr>";
+		// Add row of headers
+		for (const header of Object.keys(tableJson[0])) {
+			tableHtml += `<th>${header}</th>`
+		}
+		tableHtml += "</tr>"
 
-    for (const entry of tableJson) {
-      // For each entry in the array, create a row
-      tableHtml += "<tr>";
-      for (const column of Object.values(entry)) {
-		// If the value is an object (isArray), then nest a table
-        const value = Array.isArray(column) ? createTable(column) : column;
-        tableHtml += `<td>${value}</td>`;
-      }
-      tableHtml += "</tr>";
-    }
+		for (const entry of tableJson) {
+			// For each entry in the array, create a row
+			tableHtml += "<tr>"
+			for (const column of Object.values(entry)) {
+				// If the value is an object (isArray), then nest a table
+				const value = Array.isArray(column) ? createTable(column) : column
+				tableHtml += `<td>${value}</td>`
+			}
+			tableHtml += "</tr>"
+		}
 
-    tableHtml += "</table>";
-    return tableHtml;
-  };
+		tableHtml += "</table>"
+		return tableHtml
+	}
 
-  return createTable(tableJson);
-};
+	return createTable(tableJson)
+}
