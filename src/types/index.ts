3826e98fdb628bc451a54412e09691759032421d
psycake <52770775+psycake@users.noreply.github.com>
2023-05-15 21:37:25 -0600

    Add new interfaces to interface index

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,35 @@
 import { iBrand } from "./interfaces/brand"
+import { iBrandComment } from "./interfaces/brandComment"
+import { iBrandPost } from "./interfaces/brandPost"
 import { iBrandSocial } from "./interfaces/brandSocial"
+import { iCountry } from "./interfaces/country"
+import { iInteraction } from "./interfaces/interaction"
+import { iInteractionType } from "./interfaces/interactionType"
+import { iLexical } from "./interfaces/lexical"
 import { iSocialNetwork } from "./interfaces/socialNetwork"
 import { iTier } from "./interfaces/tier"
+import { iUser } from "./interfaces/user"
+import { iUserBrand } from "./interfaces/userBrand"
+import { iUserComment } from "./interfaces/userComment"
+import { iUserPost } from "./interfaces/userPost"
+import { iUserSocial } from "./interfaces/userSocial"
+import { iWordTrend } from "./interfaces/wordTrend"
 
 export {
 	iBrand,
+	iBrandComment,
+	iBrandPost,
 	iBrandSocial,
+	iCountry,
+	iInteraction,
+	iInteractionType,
+	iLexical,
 	iSocialNetwork,
-	iTier
+	iTier,
+	iUser,
+	iUserBrand,
+	iUserComment,
+	iUserPost,
+	iUserSocial,
+	iWordTrend
 }
